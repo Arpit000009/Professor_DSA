45428760725c92504643c3780702d0ae2191266d
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-07-07 17:30:27 +0530

    1_to_n

--- recursion/02.cpp
+++ recursion/02.cpp
@@ -9,9 +9,17 @@ void print_till_n(int n){
     print_till_n(n-1);
 }
 
+void print_1_to_n(int n){
+    if(n<1){
+        return;
+    }
+    print_1_to_n(n-1);
+    cout<<n<<endl;
+}
+
 int main(){
     int n;
     cin>>n;
-
-    print_till_n(n);
+    print_1_to_n(n);
+    // print_till_n(n);
 }
